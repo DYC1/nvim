89ec66f3174ee3d865b86389f06ac0a7f3745412
dyc <dyc1go@outlook.com>
2023-04-20 20:33:44 +0800

    .

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,4 +21,8 @@ return require('packer').startup(function(use)
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   --use 'nvim-treesitter/nvim-treesitter'
   --use 'nvim-treesitter/playground'
+  use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+	}
 end)
