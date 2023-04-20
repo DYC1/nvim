d39c0f5e908d2acc37e63f4265b472ab077ec47e
dyc <dyc1go@outlook.com>
2023-04-20 18:55:14 +0800

    init

---
+++ lua/plugins.lua
@@ -0,0 +1,24 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'glepnir/zephyr-nvim'
+  use {
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons'
+    }
+  use 'SirVer/ultisnips'
+  use 'honza/vim-snippets'
+  use 'arcticicestudio/nord-vim'
+  use 'lervag/vimtex'
+  use {
+ 	  'neoclide/coc.nvim',
+	  branch='release'
+   }
+  use 'github/copilot.vim'
+  use 'untitled-ai/jupyter_ascending.vim'
+  use "EdenEast/nightfox.nvim"
+  use 'jiangmiao/auto-pairs'
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  --use 'nvim-treesitter/nvim-treesitter'
+  --use 'nvim-treesitter/playground'
+end)
